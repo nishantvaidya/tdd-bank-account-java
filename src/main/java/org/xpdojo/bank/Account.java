a54c7e51e0efd6b710b9ef75043f86bda241e8ce
Nishant Vaidya <nishant.mca2005@gmail.com>
2022-01-28 19:26:59 +0530

    Added test for withdrawMoney

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,8 +1,11 @@
 package org.xpdojo.bank;
 
+import java.util.HashMap;
+
 public class Account {
 
     private int money;
+
 
     public Account(int money){
          this.money = money;
--- src/test/java/org/xpdojo/bank/AccountTest.java
+++ src/test/java/org/xpdojo/bank/AccountTest.java
@@ -6,17 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AccountTest {
+    private  Account account;
+
+    public AccountTest(){
+        Account account = new Account(0);
+    }
 
     @Test
     public void test_balance_check() {
 
-        Account account = new Account(10);
-        assertThat( account.balance()).isEqualTo(10);
+        assertThat( account.balance()).isEqualTo(0);
     }
 
     @Test
     public void test_deposit_money(){
-        Account account = new Account(20);
+        account.depositMoney(20);
         assertThat( account.balance()).isEqualTo(20);
     }
+
+   /* @Test
+    @Disabled
+    public void test_withDraw_money(){
+        account.withdrawMoney(10);
+        assertThat( account.balance()).isEqualTo(10);
+    }*/
 }
