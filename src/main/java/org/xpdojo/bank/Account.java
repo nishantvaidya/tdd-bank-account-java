8d4700ef5bfd5012f70489a75bd2f29c2a2f2384
Nishant Vaidya <nishant.mca2005@gmail.com>
2022-01-28 18:41:18 +0530

    Added account file

--- src/main/java/org/xpdojo/bank/Account.java
+++ src/main/java/org/xpdojo/bank/Account.java
@@ -1,4 +1,14 @@
 package org.xpdojo.bank;
 
 public class Account {
+
+    private int money;
+
+    public Account(int money){
+         this.money = money;
+    }
+
+    public int balance(){
+        return money;
+    }
 }
--- src/main/java/org/xpdojo/bank/Money.java
+++ src/main/java/org/xpdojo/bank/Money.java
@@ -1,8 +1,20 @@
 package org.xpdojo.bank;
 
+import java.math.BigDecimal;
+
 /**
  * Immutable class to represent Money as a concept.
  * This class should have no accessor methods.
  */
 public class Money {
+
+    private final int money;
+
+    public Money(int money){
+        this.money = money;
+    }
+
+    public int getMoney(){
+        return  money;
+    }
 }
